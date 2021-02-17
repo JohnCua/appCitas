046d190b35c4018f244c980ba79bc2abf96ca74a
JohnCua <cuatumax@gmail.com>
2021-02-16 19:22:47 -0600

    listar y eliminar citas

--- App.js
+++ App.js
@@ -1,114 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  FlatList,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+//componentes
+import Cita from './src/componentes/Cita';
 
-const App: () => React$Node = () => {
+
+const App = () =>  {
+
+  const [citas, setCitas] = useState([
+    {id: "1", paciente: "Hook", propietario: "Juan", sintomas: "No come"},
+    {id: "2", paciente: "Redux", propietario: "Carlos", sintomas: "No duerme"},
+    {id: "3", paciente: "Native", propietario: "Jouse", sintomas: "No canta"}
+  ]);
+
+  const eliminarPaciente = id => {
+    setCitas( (citasActuales) => {
+      return citasActuales.filter( cita => cita.id !== id);
+    } )
+  }
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={styles.contenedor}>
+      <Text style={styles.titulo}>Administrador de citas</Text>
+
+      <Text style={styles.titulo}>{citas.length > 0 ? 'Administra tus citas' : 'Agrega una cita'}</Text>
+
+      <FlatList 
+        data = { citas }
+        renderItem = { ( {item} ) => <Cita cita={item} eliminarPaciente={eliminarPaciente}/> }
+        keyExtractor = { cita => cita.id }
+      />
+
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  contenedor: {
+    backgroundColor: '#AA076B',
+    flex:1
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  titulo: {
+    marginTop:40,
+    marginBottom: 10,
+    fontSize: 20,
+    fontWeight:'bold',
+    textAlign: 'center',
+    color: "#ffff"
+  }
+
 });
 
 export default App;
